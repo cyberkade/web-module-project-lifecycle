4875710b6556486c0e36aed5c203ac1144106124
Kade Griffith <kadeg95@gmail.com>
2021-08-31 12:48:41 -0600

    got other followers to appear on page, however some fields arent being filled out. going to look into it

--- life-cycle/src/App.js
+++ life-cycle/src/App.js
@@ -11,29 +11,22 @@ class App extends React.Component {
 
   componentDidMount() {
     axios.get(`https://api.github.com/users/cyberkade`)
-    .then(res => this.setState({...this.state, userData: [...this.state.userData, res.data]}))
-    .catch(err => console.log(err))
-    .catch(err => console.log(err))
+      .then(res => this.setState({...this.state, userData: [...this.state.userData, res.data]}))
+    axios.get('https://api.github.com/users/cyberkade/followers')
+      .then(res => res.data.forEach(follower => this.setState({...this.state, userData: [...this.state.userData, follower]})))
+      .then(res => res.data.forEach(follower => this.setState({...this.state, followers: [...this.state.followers, follower.login]})))
+      // .then(res => this.setState({...this.state, userData: [...this.state.userData, res.data]}))
+      .catch(err => console.log(err))
   }
   
-
   // handleClick() {
   //   axios.get('https://api.github.com/users/cyberkade/followers')
   //   .then(res => console.log("Followers Data", res.data))
-  //   .then(res => this.setState({...this.state,
-  //       userData:[...this.state.userData, res.data]
-  //   }))
+  //   .then(res => res.data.map(this.setState({...this.state,
+  //       followers:[...this.state.followers, res.data.login]
+  //   })))
   //   .catch(err => console.log(err))
   // }
-
-  componentDidUpdate(prevState) {
-    if(prevState.userData !== this.state.userData) {
-      axios.get('https://api.github.com/users/cyberkade/followers')
-      .then(res => console.log("Followers Data", res.data))
-      .then(res => this.setState({...this.state, userData:[...this.state.userData, res.data]}))
-      .catch(err => console.log(err))
-    }
-  }
 
   // fetchUpdatedData() {
   //   axios.get(`https://api.github.com/users/${}`)
@@ -44,7 +37,18 @@ class App extends React.Component {
   render () {
     return <div className="App">
       <header className="App-header">
-        {this.state.userData.map( (user, idx) => <Card handleClick={this.handleClick} key={idx} userData={user} />)}
+        {this.state.userData.map( (user, idx) => 
+        <Card
+          handleClick={this.handleClick}
+          key={idx}
+          name={user.name}
+          login={user.login}
+          url={user['avatar_url']}
+          link={user['html_url']}
+          followers={user.followers}
+          following={user.following}
+        />)}
+        <button onClick={ () => this.handleClick()} >Show More</button>
       </header>
     </div>
   };
