b5e5559ab1f6677c8f623f68fec530f45d95b843
Kade Griffith <kadeg95@gmail.com>
2021-08-31 11:34:11 -0600

    got a github user card on page, going to get my followers and put them on page as well

--- life-cycle/src/App.js
+++ life-cycle/src/App.js
@@ -4,25 +4,49 @@ import Card from './Components/Card';
 import axios from 'axios';
 
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      userData: []
+    state = {
+      userData: [],
+      followers: []
     };
+
+  componentDidMount() {
+    axios.get(`https://api.github.com/users/cyberkade`)
+    .then(res => this.setState({...this.state, userData: [...this.state.userData, res.data]}))
+    .catch(err => console.log(err))
+    .catch(err => console.log(err))
+  }
+  
+
+  // handleClick() {
+  //   axios.get('https://api.github.com/users/cyberkade/followers')
+  //   .then(res => console.log("Followers Data", res.data))
+  //   .then(res => this.setState({...this.state,
+  //       userData:[...this.state.userData, res.data]
+  //   }))
+  //   .catch(err => console.log(err))
+  // }
+
+  componentDidUpdate(prevState) {
+    if(prevState.userData !== this.state.userData) {
+      axios.get('https://api.github.com/users/cyberkade/followers')
+      .then(res => console.log("Followers Data", res.data))
+      .then(res => this.setState({...this.state, userData:[...this.state.userData, res.data]}))
+      .catch(err => console.log(err))
+    }
   }
 
-componentDidMount() {
-  axios.get('https://api.github.com/users/cyberkade')
-  .then(res => console.log(res.data))
-  .catch(err => console.log(err))
-}
+  // fetchUpdatedData() {
+  //   axios.get(`https://api.github.com/users/${}`)
+  //   .then(res => this.setState({...this.state, userData: [...this.state.userData, res.data]}))
+  //   .catch(err => console.log(err))
+  // }
 
   render () {
     return <div className="App">
-              <header className="App-header">
-                <Card />
-              </header>
-            </div>
+      <header className="App-header">
+        {this.state.userData.map( (user, idx) => <Card handleClick={this.handleClick} key={idx} userData={user} />)}
+      </header>
+    </div>
   };
 }
 
--- life-cycle/src/Components/Card.js
+++ life-cycle/src/Components/Card.js
@@ -1,6 +1,32 @@
+import React from "react";
+import axios from 'axios';
 
-const Card = () => {
-    return null;
+class Card extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+          followers: []
+        };
+    }
+
+
+    render () {
+        console.log(this.props.userData)
+        return <div className="userCard">
+            <img className='profileImg' src={this.props.userData['avatar_url']}  alt='user git hub profile pic'/>
+            <h2>{this.props.userData.name}</h2>
+            <img className='calenderImg' src={`https://ghchart.rshah.org/${this.props.userData.login}`}  alt='user git hub profile pic'/>
+
+            <div className='cardInfo'>
+                <h3>{this.props.userData.login}'s GitHub:</h3>
+                <a href={this.props.userData['html_url']}>{this.props.userData['html_url']}</a>
+                <h3>Followers: {this.props.userData.followers}</h3>
+                <h3>Following: {this.props.userData.following}</h3>
+            </div>
+            <button onClick={this.props.handleClick} >Show More</button>
+        </div>
+    }
 }
 
 export default Card
